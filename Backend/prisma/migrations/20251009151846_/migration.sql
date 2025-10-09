52cc39814b250b6bd29f43c31290d24ab2f74960
hsn1o <96138407+hsn1o@users.noreply.github.com>
2025-10-09 23:31:33 +0800

    migration - Hasan

---
+++ Backend/prisma/migrations/20251009151846_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `proposedMilestones` on the `Proposal` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Proposal" DROP COLUMN "proposedMilestones";
